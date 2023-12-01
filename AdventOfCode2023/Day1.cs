b0ac628714ddffe9569d4b1603991a856603722b
Marius Vladu <marius.vladu96@gmail.com>
2023-12-01 20:46:38 +0200

    part2

--- AdventOfCode2023/Day1.cs
+++ AdventOfCode2023/Day1.cs
@@ -17,4 +17,52 @@ public class Day1
 
         return $"sum={sum}";
     }
+
+    public static string Part2(string[] input)
+    {
+        var sum = 0;
+
+        foreach (var line in input)
+        {
+            var digits = ExtractDigits(line);
+            if (digits.Count == 0)
+                throw new InvalidOperationException($"The following line does not contain any digit: {line}");
+
+            var number = int.Parse($"{digits.First()}{digits.Last()}");
+            sum += number;
+        }
+
+        return $"sum={sum}";
+    }
+
+    private static readonly string[] Digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    private static List<int> ExtractDigits(string line)
+    {
+        var foundDigits = new List<int>();
+
+        for (var i = 0; i < line.Length; i++)
+        {
+            if (char.IsDigit(line[i]))
+            {
+                foundDigits.Add(line[i] - '0');
+                continue;
+            }
+
+            for (int j = 0; j < Digits.Length; j++)
+            {
+                var digit = Digits[j];
+
+                var possibleDigit = line.Substring(i, Math.Min(digit.Length, line.Length - i));
+                if (possibleDigit == digit)
+                {
+                    foundDigits.Add(j);
+                    i += digit.Length - 2;
+                    break;
+                }
+            }
+        }
+
+        return foundDigits;
+    }
+
 }
