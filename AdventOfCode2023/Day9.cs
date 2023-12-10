22149cc4b54b5997200c3f29b19fb677bfa79f99
Marius Vladu <marius.vladu96@gmail.com>
2023-12-10 11:51:09 +0200

    day9 part1

---
+++ AdventOfCode2023/Day9.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode2023;
+
+public class Day9
+{
+    public static string Part1(string[] input)
+    {
+        long sum = 0;
+
+        foreach (var line in input)
+        {
+            var readings = line.Split(' ').Select(int.Parse).ToList();
+
+            var sequences = new List<List<int>> { readings };
+            var currentSequence = readings;
+
+            while (currentSequence.Any(x => x != 0))
+            {
+                var differencesSequence = new List<int>();
+
+                for (int i = 0; i < currentSequence.Count - 1; i++)
+                {
+                    differencesSequence.Add(currentSequence[i + 1] - currentSequence[i]);
+                }
+
+                sequences.Add(differencesSequence);
+
+                currentSequence = sequences.Last();
+            }
+
+            sequences.Last().Add(0);
+
+            for (int i = sequences.Count - 2; i >= 0; i--)
+            {
+                var prediction = sequences[i + 1].Last() + sequences[i].Last();
+                sequences[i].Add(prediction);
+            }
+
+            sum += sequences[0].Last();
+        }
+
+        return sum.ToString();
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -4,6 +4,6 @@
 var input = File.ReadAllLines("input.txt");
 ArgumentNullException.ThrowIfNull(input);
 
-var result = Day8.Part2(input);
+var result = Day9.Part1(input);
 
 Console.WriteLine(result);
